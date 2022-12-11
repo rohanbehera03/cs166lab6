b7c66b12771aab3108ce81323de2cc06d3cfda99
Rohan Behera <65427904+rohanbehera03@users.noreply.github.com>
2022-12-10 17:43:51 -0800

    Delete lab6-1 directory

--- lab6-1/lab6/Sample.java
+++
@@ -1,9 +0,0 @@
-public class Sample {
-    public static void main(String args[]) {
-        String str1 = "hello";
-        String str2 = "world";
-
-        String finalString = str1 + str2;
-        System.out.println(finalString);
-    }
-}
